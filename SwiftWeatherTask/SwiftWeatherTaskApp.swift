1ecf3c83fbf56ea8c8a63c7a02b6a1abd849e003
Ahmed-Abdelraouf <eng.ahmed.elsaadny@gmail.com>
2023-06-11 10:47:56 +0300

    Initial Commit

---
+++ SwiftWeatherTask/SwiftWeatherTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherTaskApp.swift
+//  SwiftWeatherTask
+//
+//  Created by Oufaa on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
